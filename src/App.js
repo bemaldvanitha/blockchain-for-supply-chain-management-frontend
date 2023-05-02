521181b48f35c0ec7ee3331df67b31ee0f55a41d
bemaldvanitha <bdvidanagamachchi@gmail.com>
2023-05-02 10:23:35 +0530

    router setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 import ProductOwnerAddScreen from "./pages/ProductOwnerAdd";
 import ProductOwnerScreen from "./pages/ProductOwnerScreen";
+import OwnerOwnedProductScreen from "./pages/OwnerOwnedProductScreen";
+import SelectedProductScreen from "./pages/SelectedProductScreen";
+import AddProductScreen from "./pages/AddProduct";
 
 function App() {
   return (
-    <div>
-      <ProductOwnerScreen/>
-    </div>
+      <div>
+        <Routes>
+            <Route path={'/'} element={ <ProductOwnerScreen/> }/>
+            <Route path={'/add-owner'} element={ <ProductOwnerAddScreen/> }/>
+            <Route path={'/owner/:id'} element={ <OwnerOwnedProductScreen/> }/>
+            <Route path={'/product/:id'} element={ <SelectedProductScreen/> }/>
+            <Route path={'/add-product'} element={ <AddProductScreen/> }/>
+        </Routes>
+      </div>
   );
 }
 
